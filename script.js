44bb8cff7eedf0f0074c1873e093757e34d8208a
ragahrafdariselatan <ragahraf@gmail.com>
2025-06-20 23:02:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ function createDigitColumn(id, digits) {
   });
 }
 
-// Setup visible digit boxes for hours (12 boxes), minutes (16), and seconds (16)
 createDigitColumn("h1", [0, 1]);
 createDigitColumn("h2", [0,1,2,3,4,5,6,7,8,9]);
 createDigitColumn("m1", [0,1,2,3,4,5]);
